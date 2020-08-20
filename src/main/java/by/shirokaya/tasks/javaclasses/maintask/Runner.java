9c86569083314fc97b83897229f29f3f390e5e4c
Halina-Shyrokaya <Halina.Shyrokaya@itechart-group.com>
2020-08-20 22:53:26 +0300

    'java classes' main task added

---
+++ src/main/java/by/shirokaya/tasks/javaclasses/maintask/Runner.java
@@ -0,0 +1,73 @@
+package by.shirokaya.tasks.javaclasses.maintask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        List<Car> cars = new ArrayList<>();
+
+        cars.add(new CarBuilder()
+                .withModel("Tiida")
+                .withBrand("Nissan")
+                .withPrice(3999.90)
+                .withColor("black")
+                .withYearOfIssue(2008)
+                .withRegistrationNumber("5530KT7")
+                .build());
+
+        cars.add(new CarBuilder()
+                .withModel("Tiida")
+                .withBrand("Nissan")
+                .withPrice(5694.6)
+                .build());
+
+        cars.add(new CarBuilder()
+                .withModel("Almera")
+                .withBrand("Nissan")
+                .withColor("black")
+                .withYearOfIssue(1998)
+                .build());
+
+        cars.add(new CarBuilder()
+                .withBrand("Lexus")
+                .withPrice(9999)
+                .withColor("white")
+                .withYearOfIssue(2010)
+                .withRegistrationNumber("5890KT7")
+                .build());
+
+        cars.add(new CarBuilder()
+                .withBrand("Lexus")
+                .withPrice(4500.0)
+                .withColor("white")
+                .withYearOfIssue(2008)
+                .withRegistrationNumber("5890KT7")
+                .build());
+
+        List<Car> result;
+
+        System.out.println("a) список автомобилей заданной марки: ");
+        result = CarService.filterByBrand(cars, "Nissan");
+        printResultList(result);
+
+        System.out.println("b) список автомобилей заданной модели, которые эксплуатируются больше n лет;");
+        result = CarService.filterByModelAndLifespan(cars, "Tiida", 5);
+        printResultList(result);
+
+        System.out.println("c) список автомобилей заданного года выпуска, цена которых больше указанной.");
+        result = CarService.filterByYearAndMinPrice(cars, 2008, 3500.0);
+        printResultList(result);
+    }
+
+    private static void printResultList(List<Car> result) {
+        if (result.isEmpty()) {
+            System.out.println("No results");
+        } else {
+            for (Car car : result) {
+                System.out.println(car.toString());
+            }
+        }
+    }
+}
